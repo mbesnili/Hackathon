340a1b87636156edf3bda06d728d60896c779c6d
mbesnili <besnilimustafa@gmail.com>
2018-02-16 12:50:13 +0300

    Create project, initialize podfile.

---
+++ Hackathon/Hackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackathon
+//
+//  Created by mbesnili on 16.02.2018.
+//  Copyright © 2018 mbesnili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
