240aaee6d3ddf1fa973a5d2f0fd5826365bad4ed
mbesnili <besnilimustafa@gmail.com>
2018-02-17 02:41:42 +0300

    Replace Permission with Permission/Location as only location required. Add necessary keys to InfoPlist.string.

--- Hackathon/Hackathon/ViewControllers/PackageListViewController.swift
+++ Hackathon/Hackathon/ViewControllers/PackageListViewController.swift
@@ -6,11 +6,16 @@
 //  Copyright © 2018 mbesnili. All rights reserved.
 //
 
+import Permission
 import UIKit
 
 class PackageListViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let permission: Permission = .locationAlways
+        permission.request { _ in
+        }
     }
 }
