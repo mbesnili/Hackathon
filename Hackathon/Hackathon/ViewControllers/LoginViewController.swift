011280893dba0973ba12f33b732d4fbf6e509d7f
mbesnili <besnilimustafa@gmail.com>
2018-02-17 00:13:36 +0300

    Rename Main.storyboards to Login.storyboards. Add LoginViewController.

---
+++ Hackathon/Hackathon/ViewControllers/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Hackathon
+//
+//  Created by mbesnili on 16.02.2018.
+//  Copyright © 2018 mbesnili. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewController: BaseViewController {
+    
+}
