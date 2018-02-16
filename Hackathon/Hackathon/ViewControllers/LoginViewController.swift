7088c0f3e6ff3595b3fd3dca18a776fb9d24c2cb
mbesnili <besnilimustafa@gmail.com>
2018-02-17 00:36:10 +0300

    Implement login view controller.

--- Hackathon/Hackathon/ViewControllers/LoginViewController.swift
+++ Hackathon/Hackathon/ViewControllers/LoginViewController.swift
@@ -7,7 +7,18 @@
 //
 
 import Foundation
+import UIKit
 
 class LoginViewController: BaseViewController {
-    
+
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        usernameTextField.placeholder = R.string.localization.loginUsernamePlaceholderText()
+        passwordTextField.placeholder = R.string.localization.loginPasswordPlaceholderText()
+        loginButton.setTitle(R.string.localization.loginLoginButtonTitle(), for: [])
+    }
 }
